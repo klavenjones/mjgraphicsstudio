b38920b8e065c05fa5e912632316dc968290219b
Klaven Jones <klavensjones@gmail.com>
2021-06-23 15:20:13 -0400

    Worked on navigation menu

--- components/shared/navigation.js
+++ components/shared/navigation.js
@@ -1,7 +1,80 @@
 import React from 'react'
+import Link from 'next/link'
+
+import {
+  FaDribbble,
+  FaInstagram,
+  FaTwitter,
+  FaBehanceSquare
+} from 'react-icons/fa'
+
+const navigation = [
+  {
+    href: '/',
+    label: 'Work'
+  },
+  {
+    href: '/about',
+    label: 'About'
+  },
+  {
+    href: '/shop',
+    label: 'Shop'
+  }
+]
+
+const socialNavigation = [
+  {
+    icon: <FaInstagram className='icon' />,
+    href: 'https://instagram.com'
+  },
+  {
+    icon: <FaTwitter className='icon' />,
+    href: 'https://instagram.com'
+  },
+  {
+    icon: <FaDribbble className='icon' />,
+    href: 'https://instagram.com'
+  },
+  {
+    icon: <FaBehanceSquare className='icon' />,
+    href: 'https://instagram.com'
+  }
+]
 
 function Navigation() {
-  return <div></div>
+  return (
+    <>
+      <div className='navbar'>
+        <div className='nav-left'>
+          {navigation.map((link, i) => (
+            <Link href={link.href}>
+              <a>{link.label}</a>
+            </Link>
+          ))}
+        </div>
+        <div className='logo'>
+          <a href=''>Logo</a>
+        </div>
+        <div className='nav-right'>
+          {socialNavigation.map((link, i) => (
+            <Link href={link.href}>
+              <a>{link.icon}</a>
+            </Link>
+          ))}
+        </div>
+      </div>
+      {/* Mobile Menu */}
+      <div className='navbar-mobile'>
+        <div className='logo'>
+          <a href=''>Logo</a>
+        </div>
+        <div className='nav-right'>
+          <a href=''>Hamburger Menu</a>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Navigation
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Navigation from '../components/shared/navigation'
 
 export default function Home() {
   return (
     <div>
+      <Navigation />
       <h1>HOME</h1>
     </div>
   )
