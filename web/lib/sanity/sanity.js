3a8c91a0e6f17bfa8dcd6f7f57a39c012540a126
klavenjones <klavensjones@gmail.com>
2021-07-06 13:31:04 -0400

    Added Env Variables

--- web/lib/sanity/sanity.js
+++ web/lib/sanity/sanity.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: 'v2021-06-07',
   useCdn: true
